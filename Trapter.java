5f0d430e3ea2da147baffbc6c7f7bef780e359f4
Zsomi4 <zsomidev@gmail.com>
2022-02-22 08:47:44 +0100

    Fejléc és kiíratások megírása.

--- Trapter.java
+++ Trapter.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 class Trapter {
 	public static void main(String[] args) {
+		System.out.println("Gubis Zsombor Dániel, Szoft_I_N");
 		try (Scanner scan = new Scanner(System.in)) {
 
+			System.out.print("A oldal: ");
+			double aoldal = scan.nextDouble();
+
+			System.out.print("B oldal: ");
+			double boldal = scan.nextDouble();
+
+			System.out.print("C oldal: ");
+			double coldal = scan.nextDouble();
 		}
 	}
 }
